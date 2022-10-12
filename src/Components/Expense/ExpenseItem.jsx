b7fa87ff78e72c43396516cda57d20d3ef5b6524
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2022-10-12 10:26:28 +0700

    day2

--- src/Components/Expense/ExpenseItem.jsx
+++ src/Components/Expense/ExpenseItem.jsx
@@ -1,19 +1,32 @@
+import React, { useState } from 'react';
 import '../Expense/ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
+
 const ExpenseItem = (props) => {
+    //fuction Click Handler()
+
+    const [title, setTitle] = useState(props.title)
+    const handleClick = () => {
+        setTitle('Update Success!!');
+    }
 
     return (
 
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>
                     ${props.amount}
                 </div>
             </div>
+            <button className='btn__submit--primary'
+                onClick={handleClick}
+            >
+                ChangeTitle
+            </button>
         </Card>
 
     );
